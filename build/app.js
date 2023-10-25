532fa20ec3f638d76ca9f1d1a800c21de311dbf3
Alessandro Torcetta <a.torcetta@reply.it>
2023-10-26 00:14:18 +0200

    refs #challenge - implementazione factory pattern

--- build/app.js
+++ build/app.js
@@ -3,20 +3,75 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var readlineSync = require("readline-sync");
 var functions_1 = require("./functions");
+var WordsFactory = /** @class */ (function () {
+    function WordsFactory() {
+    }
+    WordsFactory.prototype.createElements = function () {
+        return new ConcreteWords();
+    };
+    return WordsFactory;
+}());
+var LettersFactory = /** @class */ (function () {
+    function LettersFactory() {
+    }
+    LettersFactory.prototype.createElements = function () {
+        return new ConcreteLetters();
+    };
+    return LettersFactory;
+}());
+var SpacesFactory = /** @class */ (function () {
+    function SpacesFactory() {
+    }
+    SpacesFactory.prototype.createElements = function () {
+        return new ConcreteSpaces();
+    };
+    return SpacesFactory;
+}());
+var ConcreteWords = /** @class */ (function () {
+    function ConcreteWords() {
+    }
+    ConcreteWords.prototype.create = function (fileContent) {
+        var regexWords = /\b\w+\b/g;
+        var wordList = fileContent.match(regexWords);
+        console.log("Numero parole: " + wordList.length);
+        return wordList;
+    };
+    return ConcreteWords;
+}());
+var ConcreteLetters = /** @class */ (function () {
+    function ConcreteLetters() {
+    }
+    ConcreteLetters.prototype.create = function (fileContent) {
+        var regexLetters = /[a-zA-Z]/g;
+        var letterList = fileContent.match(regexLetters);
+        console.log("Numero lettere: " + letterList.length);
+        return letterList;
+    };
+    return ConcreteLetters;
+}());
+var ConcreteSpaces = /** @class */ (function () {
+    function ConcreteSpaces() {
+    }
+    ConcreteSpaces.prototype.create = function (fileContent) {
+        var regexSpaces = /\s/g;
+        var spacesList = fileContent.match(regexSpaces);
+        console.log("Numero spazi: " + spacesList.length);
+        return spacesList;
+    };
+    return ConcreteSpaces;
+}());
+function process(factory, fileContent) {
+    var elements = factory.createElements();
+    return elements.create(fileContent);
+}
 function main() {
     var userInput = readlineSync.question('Inserisci il path del file (locale o un URL):');
     console.log("Path inserito:", userInput);
     var words = fs.readFileSync(userInput, 'utf-8');
-    var regexWords = /\b\w+\b/g;
-    var regexLetters = /[a-zA-Z]/g;
-    var regexSpaces = /\s/g;
-    var wordList = words.match(regexWords);
-    var letterList = words.match(regexLetters);
-    var spacesList = words.match(regexSpaces);
+    var wordList = process(new WordsFactory(), words);
+    process(new LettersFactory(), words);
+    process(new SpacesFactory(), words);
     var repeatedWords = (0, functions_1.find10OccurWords)(Array.from(wordList));
-    console.log("Numero parole: " + wordList.length);
-    console.log("Numero lettere: " + letterList.length);
-    console.log("Numero spazo: " + spacesList.length);
     console.log("Parole che si ripetono più di 10 volte: " + repeatedWords);
     console.log("Numero parole che si ripetono più di 10 volte: " + repeatedWords.length);
 }
--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,76 @@ import * as fs from 'fs';
 import * as readlineSync from 'readline-sync';
 import {find10OccurWords} from "./functions";
 
+/**
+ * The Abstract Factory interface declares a set of methods that return
+ * abstract elements.
+ */
+interface AbstractFactory {
+    createElements(): AbstractElements;
+}
+
+class WordsFactory implements AbstractFactory{
+    public createElements(): AbstractElements {
+        return new ConcreteWords();
+    }
+}
+
+class LettersFactory implements AbstractFactory{
+    public createElements(): AbstractElements {
+        return new ConcreteLetters();
+    }
+}
+
+class SpacesFactory implements AbstractFactory{
+    public createElements(): AbstractElements {
+        return new ConcreteSpaces();
+    }
+}
+
+interface AbstractElements {
+    create(fileContent: string): RegExpMatchArray;
+}
+
+class ConcreteWords  implements AbstractElements{
+    public create(fileContent: string): RegExpMatchArray{
+        const regexWords = /\b\w+\b/g;
+        const wordList = fileContent.match(regexWords);
+        console.log("Numero parole: " + wordList.length);
+        return wordList;
+    }
+}
+
+class ConcreteLetters implements AbstractElements {
+    public create(fileContent: string): RegExpMatchArray{
+        const regexLetters = /[a-zA-Z]/g;
+        const letterList = fileContent.match(regexLetters);
+        console.log("Numero lettere: " + letterList.length);
+        return letterList;
+    }
+}
+
+class ConcreteSpaces implements AbstractElements {
+    public create(fileContent: string): RegExpMatchArray{
+        const regexSpaces = /\s/g;
+        const spacesList = fileContent.match(regexSpaces);
+        console.log("Numero spazi: " + spacesList.length);       
+        return spacesList;
+    }
+}
+
+function process(factory: AbstractFactory, fileContent:string): RegExpMatchArray{
+    const elements = factory.createElements();
+    return elements.create(fileContent);
+}
+
 function main(): void {
     const userInput = readlineSync.question('Inserisci il path del file (locale o un URL):');
     console.log("Path inserito:", userInput);
     const words = fs.readFileSync(userInput, 'utf-8');
-    const regexWords = /\b\w+\b/g;
-    const regexLetters = /[a-zA-Z]/g;
-    const regexSpaces = /\s/g;
-    const wordList = words.match(regexWords);
-    const letterList = words.match(regexLetters);
-    const spacesList = words.match(regexSpaces);
+    const wordList = process(new WordsFactory(), words);
+    process(new LettersFactory(), words);
+    process(new SpacesFactory(), words);
     const repeatedWords = find10OccurWords(Array.from(wordList));
-    console.log("Numero parole: " + wordList.length);
-    console.log("Numero lettere: " + letterList.length);
-    console.log("Numero spazo: " + spacesList.length);
     console.log("Parole che si ripetono più di 10 volte: " + repeatedWords);
     console.log("Numero parole che si ripetono più di 10 volte: " + repeatedWords.length);
 }
